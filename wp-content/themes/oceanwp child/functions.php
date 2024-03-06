f318499ef862fe1d3634dd2e3df330aa6f9e2736
Frederic Claveau <fredclaveau@gmail.com>
2024-03-06 11:24:33 +0100

    avancée sur le menu

--- wp-content/themes/oceanwp child/functions.php
+++ wp-content/themes/oceanwp child/functions.php
@@ -5,15 +5,13 @@ function theme_enqueue_styles() {
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
-// function add_extra_item_to_nav_menu($items,$args){
-//     var_dump($args->menu);
-//     exit;
-//     if(is_user_logged_in() && $args->menu ==2) {
-//         $items .= '<li class="menu_item"><a href="'.get_site_url().'/wp-admin'.'">admin</a></li>';
-//     }
-//     return $items;
-// }
+function add_extra_item_to_nav_menu($items, $args) {
+    if (is_user_logged_in() && $args->theme_location == 'primary') {
+        $items .= '<li class="menu_item"><a href="' . admin_url() . '">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2);
 
-// add_filter('wp_nav_menu_items', 'add_extra_items_to_nav_menu',10,2);
 
 
